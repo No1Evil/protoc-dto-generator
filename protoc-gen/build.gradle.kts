e18a51eaf7719d7dd037d8805dbd09796639c14e
NotEvil <notevil036@gmail.com>
2026-04-09 06:39:02 +0300

    chore(protoc-gen): initialize protoc-gen module

---
+++ protoc-gen/build.gradle.kts
@@ -0,0 +1,59 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "9.4.1"
+    id("org.graalvm.buildtools.native") version "1.0.0"
+    `kotlin-dsl`
+}
+
+group = "io.github.no1evil"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.28.0")
+    implementation("com.google.protobuf:protobuf-java:4.34.1")
+    implementation("com.google.protobuf:protobuf-java-util:4.34.1")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    compileOnly("org.projectlombok:lombok:1.18.44")
+    annotationProcessor("org.projectlombok:lombok:1.18.44")
+}
+
+tasks.jar {
+    enabled = false
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+
+    manifest {
+        attributes["Main-Class"] = "io.github.no1evil.protogen.ProtoGen"
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+        vendor.set(JvmVendorSpec.GRAAL_VM)
+    }
+}
+
+graalvmNative {
+
+    binaries {
+        named("main") {
+            verbose.set(true)
+            buildArgs.add("--no-fallback")
+            buildArgs.add("-H:+ReportExceptionStackTraces")
+            buildArgs.add("--initialize-at-build-time=com.google.protobuf")
+            buildArgs.add("--initialize-at-run-time=com.github.javaparser")
+        }
+    }
+}
